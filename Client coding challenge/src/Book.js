0c55bf80ccbf17bbca6956b943bcc61be7aa6e76
aglahir <aglahirjf@gmail.com>
2020-06-05 10:01:59 -0500

    A01364026

--- Client coding challenge/src/Book.js
+++ Client coding challenge/src/Book.js
@@ -1,13 +1,17 @@
 import React from "react";
 
 function Book(props) {
-  return (
-    <div>
-      {props.req.items.map((value, index) => {
-        return <p>{value.volumeInfo.title}</p>;
-      })}
-    </div>
-  );
+  if (props) {
+    return (
+      <div>
+        {props.req.items.map((value, index) => {
+          return <p>{value.volumeInfo.title}</p>;
+        })}
+      </div>
+    );
+  } else {
+    return <div></div>;
+  }
 }
 
 export default Book;
